4614c8af76c3e353c8f6fa8eff7ccc5c52094919
Marcia Feitel <marcia.feitel@gmail.com>
2014-08-24 02:12:18 -0700

    First attempt at submitted version
    Includes function file, test output (commands.Rhistory), run transcript
    (commands.output).

---
+++ README.assignmentfiles.txt
@@ -0,0 +1,7 @@
+I've included a command file and output from the command to demonstrate
+my code in action. I had some problems defining the behavior for
+'matrix has changed after inverse was cached' and included an extra
+function, a safeCacheSolve, to handle that use case.
+
+commands.Rhistory  : file of commands to check my functions
+commands.output    : output of these commands
--- cachematrix.R
+++ cachematrix.R
@@ -3,16 +3,18 @@
 #get the value of the matrix
 #set the value of the matrix inverse
 #get the value of the matrix inverse from the cache (including NULL values, on first use)
+#Note that the set function can reset the underlying matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     mInverse <- NULL
-    set <- function(y) {
-	x <<- x
+    set <- function(y) {        #save matrix, placeholder for inverse
+	x <<- y
 	mInverse <<- NULL
     }
-    get <- function() x
+    get <- function() x        #return underlying matrix
     setInverse <- function(computedInverse)  mInverse <<- computedInverse
-    getInverse <- function() mInverse
+                               #set value of inverse to computed value
+    getInverse <- function() mInverse   #return cached value
     list(set = set, get = get,
 	setInverse = setInverse,
 	getInverse = getInverse)
@@ -27,14 +29,43 @@ makeCacheMatrix <- function(x = matrix()) {
 #cache via the `setinverse` function.
 
 cacheSolve <- function(x, ...) {
-    mInverse <- x$getInverse()
-    if(!is.null(mInverse)) {
+    mInverse <- x$getInverse()  #retrieve cached value, which may be null
+    if(!is.null(mInverse)) {    #if not NULL retrieve and return inverse
 	message("getting cached data")
 	return(mInverse)
     }
-    data <- x$get()
-    mInverse <- solve(data, ...)
-    x$setInverse(mInverse)
-    mInverse
+    data <- x$get()             #if NULL--get matrix
+    mInverse <- solve(data, ...)    #compute index
+    x$setInverse(mInverse)          #set cached value
+    mInverse                        #return inverse
+}
+
+
+#The instructions told us to return the cached value if the underlying
+#matrix hasn't changed, but doesn't describe desired behavior if it has.
+#I implemented an update-and-recache approach
+#
+#Here, safeCacheSolve adresses the possibility that the matrix
+#whose inverse is cached changed after its inverse was stored.
+#Given the cachedMatrix, and checkMatrix (the matrix whose inverse was cached,
+#which may have been modified)
+#It retrieves the underlying matrix for the cached inverse object and compares
+#it to the matrix argument
+#If the matrix hasn't changed, the cached index is returned
+#If it has changed, the cachedMatrix object is updated, and the inverse
+#is recomupted and cached.
+#
+#Alternative approach: Raise an error if the matrix changed, 
+#                      otherwise, return cached value
+safeCacheSolve <-function(checkMatrix = matrix(), cachedMatrix) {
+   if (!identical(checkMatrix, cachedMatrix$get())) { #matrix has changed
+	cachedMatrix$set(checkMatrix)                 #reset matrix in
+                                                      #cachedMatrix
+        message("matrix has changed, computing new inverse")  #inform user
+        return(cacheSolve(cachedMatrix))    #recompute inverse, which
+                                            #will be cached in the call
+                                            #cacheSolve
+   }
+   cacheSolve(cachedMatrix) #cached value is valid, return it
 }
 
